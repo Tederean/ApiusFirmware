895d23a321afd7cd5b2f595bc825b6d3acdcf362
tederean <tederean@gmail.com>
2022-04-03 16:26:40 +0200

    Make scaleable for more gauges

--- src/application/gui/GuiService.cpp
+++ src/application/gui/GuiService.cpp
@@ -12,16 +12,21 @@ namespace Services
   namespace Gui
   {
 
+    typedef struct Gauge
+    {
+      lv_obj_t *Arc;
+      lv_style_t IndicatorStyle;
+      lv_style_t MainStyle;
+      lv_anim_t Animation;
+    } Gauge;
+
+
+    const size_t GaugeCount = 1;
+
+    Gauge Gauges[GaugeCount];
+
     lv_style_t ScreenStyle;
 
-
-    lv_obj_t *Arc;
-
-    lv_style_t ArcIndicatorStyle;
-
-    lv_style_t ArcMainStyle;
-
-    lv_anim_t ArcAnimation;
 
     const timespan_t ArcUpdateInterval_us = 1 * 1000 * 1000;
 
@@ -31,9 +36,7 @@ namespace Services
 
     void Initialize();
 
-    void InitializeStyle();
-
-    void DrawArc(lv_obj_t *parent);
+    void DrawArc(lv_obj_t *parent, size_t index, lv_color_t backgroundColor, lv_color_t emptyColor, lv_color_t filledColor);
 
     void OnArcUpdateEvent(void *args);
 
@@ -45,64 +48,76 @@ namespace Services
     {
       auto *display = lv_disp_get_default();
       auto *screen = lv_disp_get_scr_act(display);
-      
-      InitializeStyle();
 
-      lv_obj_add_style(screen, &ScreenStyle, LV_PART_MAIN);
-
-      DrawArc(screen);
-    }
-
-    void InitializeStyle()
-    {
       auto gray20 = lv_color_make(0x33, 0x33, 0x33);
       auto gray50 = lv_color_make(0x7F, 0x7F, 0x7F);
       auto red = lv_color_make(0xFF, 0x00, 0x00);
 
 
       lv_style_set_bg_color(&ScreenStyle, gray20);
+      lv_obj_add_style(screen, &ScreenStyle, LV_PART_MAIN);
 
-      lv_style_set_bg_color(&ArcIndicatorStyle, gray20);
-      lv_style_set_arc_color(&ArcIndicatorStyle, red);
 
-      lv_style_set_bg_color(&ArcMainStyle, gray20);
-      lv_style_set_arc_color(&ArcMainStyle, gray50);
-    }
+      DrawArc(screen, 0, gray20, gray50, red);
 
-    void DrawArc(lv_obj_t *parent)
-    {
-      Arc = lv_arc_create(parent);
-
-      lv_obj_set_size(Arc, 480, 480);
-      lv_arc_set_end_angle(Arc, 200);
-      lv_obj_align(Arc, LV_ALIGN_CENTER, 0, 0);
-      lv_arc_set_range(Arc, 0, 105);
-      lv_arc_set_value(Arc, 75);
-
-      lv_obj_remove_style(Arc, NULL, LV_PART_KNOB);
-      lv_obj_add_style(Arc, &ArcIndicatorStyle, LV_PART_INDICATOR);
-      lv_obj_add_style(Arc, &ArcMainStyle, LV_PART_MAIN);
-
-      lv_anim_init(&ArcAnimation);
 
       ArcUpdateEvent.Subscribe(OnArcUpdateEvent);
       Services::System::InvokeLater(&ArcUpdateEvent, ArcUpdateInterval_us, SchedulingBehaviour::FixedPeriodSkipTicks);
     }
 
+
+    void DrawArc(lv_obj_t *parent, size_t index, lv_color_t backgroundColor, lv_color_t emptyColor, lv_color_t filledColor)
+    {
+      auto temperatureGauge = &Gauges[index];
+      auto arc = lv_arc_create(parent);
+      auto indicatorStyle = &temperatureGauge->IndicatorStyle;
+      auto mainStyle = &temperatureGauge->MainStyle;
+      auto animation = &temperatureGauge->Animation;
+
+      temperatureGauge->Arc = arc;
+
+      lv_obj_set_size(arc, 400, 400);
+      lv_arc_set_end_angle(arc, 200);
+      lv_obj_align(arc, LV_ALIGN_CENTER, 0, 0);
+      lv_arc_set_range(arc, 0, 10500);
+      lv_arc_set_value(arc, 7500);
+
+
+      lv_style_set_bg_color(indicatorStyle, backgroundColor);
+      lv_style_set_arc_color(indicatorStyle, filledColor);
+
+      lv_style_set_bg_color(mainStyle, backgroundColor);
+      lv_style_set_arc_color(mainStyle, emptyColor);
+
+
+      lv_obj_remove_style(arc, NULL, LV_PART_KNOB);
+      lv_obj_add_style(arc, indicatorStyle, LV_PART_INDICATOR);
+      lv_obj_add_style(arc, mainStyle, LV_PART_MAIN);
+
+      lv_anim_init(animation);
+    }
+
     void OnArcUpdateEvent(void *args)
     {
-      auto minValue = lv_arc_get_min_value(Arc);
-      auto maxValue = lv_arc_get_max_value(Arc);
-      auto currentValue = lv_arc_get_value(Arc);
-      auto nextValue = random(minValue, maxValue);
+      for (size_t index = 0; index < GaugeCount; index++)
+      {
+        auto gauge = &Gauges[index];
+        auto arc = gauge->Arc;
+        auto animation = &gauge->Animation;
 
-      lv_anim_set_exec_cb(&ArcAnimation, OnAnimationSetValue); 
-      lv_anim_set_values(&ArcAnimation, currentValue, nextValue);
-      lv_anim_set_var(&ArcAnimation, Arc);
-      lv_anim_set_time(&ArcAnimation, 200);
-      lv_anim_set_path_cb(&ArcAnimation, lv_anim_path_linear);
+        auto minValue = lv_arc_get_min_value(arc);
+        auto maxValue = lv_arc_get_max_value(arc);
+        auto currentValue = lv_arc_get_value(arc);
+        auto nextValue = random(minValue, maxValue);
 
-      lv_anim_start(&ArcAnimation);
+        lv_anim_set_exec_cb(animation, OnAnimationSetValue); 
+        lv_anim_set_values(animation, currentValue, nextValue);
+        lv_anim_set_var(animation, arc);
+        lv_anim_set_time(animation, 950);
+        lv_anim_set_path_cb(animation, lv_anim_path_ease_in_out);
+
+        lv_anim_start(animation);
+      }
     }
 
     void OnAnimationSetValue(void *component, int32_t value)
