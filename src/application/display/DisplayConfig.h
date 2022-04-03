0b15a76084a301608661a888cca8be87977579a5
tederean <tederean@gmail.com>
2022-04-03 15:40:22 +0200

    Add debug configuration

--- src/application/display/DisplayConfig.h
+++ src/application/display/DisplayConfig.h
@@ -59,7 +59,7 @@ public:
       cfg.dummy_read_bits  =     1;
       cfg.readable         =  true;
       cfg.invert           = false;
-      cfg.rgb_order        = false;
+      cfg.rgb_order        =  true;
       cfg.dlen_16bit       = false;
       cfg.bus_shared       =  true;
 
