c2b125f11a9aaabfefcaaad4d6e39e87219d9d0d
tederean <tederean@gmail.com>
2022-04-11 17:15:38 +0200

    Display values

--- src/application/gui/GuiService.cpp
+++ src/application/gui/GuiService.cpp
@@ -13,19 +13,30 @@ namespace Services
   namespace Gui
   {
 
+    typedef enum RoundMode
+    {
+      Integer,
+      ThreeDigitsMegaByte,
+    };
+
     typedef struct Gauge
     {
       const char *TitleText;
       const char *Unit;
       const float MinValue;
       const float MaxValue;
-      lv_anim_exec_xcb_t UpdateCallback;
+      const RoundMode ValueRounding;
+
       lv_obj_t *Arc;
-      lv_obj_t *Title;
       lv_style_t IndicatorStyle;
       lv_style_t MainStyle;
-      lv_style_t TitleStyle;
       lv_anim_t IndicatorAnimation;
+
+      lv_obj_t *TitleLabel;
+      lv_style_t TitleStyle;
+
+      lv_obj_t *ValueLabel;
+      lv_style_t ValueStyle;
     } Gauge;
 
 
@@ -38,29 +49,13 @@ namespace Services
 
     void DrawTitles(lv_obj_t *parent, lv_color_t textColor);
 
-    void DrawArcs(lv_obj_t *parent, lv_color_t backgroundColor, lv_color_t emptyColor);
+    void DrawArcs(lv_obj_t *parent, lv_color_t backgroundColor, lv_color_t emptyColor, lv_color_t fillColor, lv_color_t textColor);
 
-    void DrawArc(lv_obj_t *parent, size_t index, lv_coord_t x, lv_coord_t y, lv_color_t backgroundColor, lv_color_t emptyColor);
+    void DrawArc(lv_obj_t *parent, size_t index, lv_coord_t x, lv_coord_t y, lv_color_t backgroundColor, lv_color_t emptyColor, lv_color_t fillColor, lv_color_t textColor);
 
     void OnArcUpdateEvent(void *args);
 
-    void OnCpuUtilizationUpdate(void *component, int32_t value);
-
-    void OnCpuWattageUpdate(void *component, int32_t value);
-
-    void OnCpuTemperatureUpdate(void *component, int32_t value);
-
-    void OnCpuMemoryUpdate(void *component, int32_t value);
-
-    void OnGpuUtilizationUpdate(void *component, int32_t value);
-
-    void OnGpuWattageUpdate(void *component, int32_t value);
-
-    void OnGpuTemperatureUpdate(void *component, int32_t value);
-
-    void OnGpuMemoryUpdate(void *component, int32_t value);
-    
-    void OnAnimationUpdate(Gauge *gaugeStruct, int32_t arcValue);
+    void OnAnimationUpdate(void *component, int32_t value);
 
 
 
@@ -75,7 +70,7 @@ namespace Services
         .Unit = "%",
         .MinValue = 0.0f,
         .MaxValue = 100.0f,
-        .UpdateCallback = OnCpuUtilizationUpdate,
+        .ValueRounding = RoundMode::Integer,
       },
 
       {
@@ -83,7 +78,7 @@ namespace Services
         .Unit = "W",
         .MinValue = 0.0f,
         .MaxValue = 105.0f,
-        .UpdateCallback = OnCpuWattageUpdate,
+        .ValueRounding = RoundMode::Integer,
       },
 
       {
@@ -91,7 +86,7 @@ namespace Services
         .Unit = "°C",
         .MinValue = 0.0f,
         .MaxValue = 105.0f,
-        .UpdateCallback = OnCpuTemperatureUpdate,
+        .ValueRounding = RoundMode::Integer,
       },
 
       {
@@ -99,7 +94,7 @@ namespace Services
         .Unit = "B",
         .MinValue = 0.0f,
         .MaxValue = 32767.0f,
-        .UpdateCallback = OnCpuMemoryUpdate,
+        .ValueRounding = RoundMode::ThreeDigitsMegaByte,
       },
 
       {
@@ -107,7 +102,7 @@ namespace Services
         .Unit = "%",
         .MinValue = 0.0f,
         .MaxValue = 100.0f,
-        .UpdateCallback = OnGpuUtilizationUpdate,
+        .ValueRounding = RoundMode::Integer,
       },
 
       {
@@ -115,7 +110,7 @@ namespace Services
         .Unit = "W",
         .MinValue = 0.0f,
         .MaxValue = 105.0f,
-        .UpdateCallback = OnGpuWattageUpdate,
+        .ValueRounding = RoundMode::Integer,
       },
 
       {
@@ -123,15 +118,15 @@ namespace Services
         .Unit = "°C",
         .MinValue = 0.0f,
         .MaxValue = 105.0f,
-        .UpdateCallback = OnGpuTemperatureUpdate,
+        .ValueRounding = RoundMode::Integer,
       },
 
       {
         .TitleText = "Memory",
         .Unit = "B",
         .MinValue = 0.0f,
-        .MaxValue = 32767.0f,
-        .UpdateCallback = OnGpuMemoryUpdate,
+        .MaxValue = 8192.0f,
+        .ValueRounding = RoundMode::ThreeDigitsMegaByte,
       },
     };
 
@@ -157,7 +152,9 @@ namespace Services
 
     const lv_coord_t ArcIndicatorSize = 20;
 
-    const lv_coord_t TitleHight = 49;
+    const lv_coord_t TitleHeight = 49;
+
+    const lv_coord_t ValueHeight = 40;
     
     const lv_coord_t GridSpacing = 17;
 
@@ -172,17 +169,33 @@ namespace Services
       auto *display = lv_disp_get_default();
       auto *screen = lv_disp_get_scr_act(display);
 
-      auto gray80 = lv_color_make(0x33, 0x33, 0x33);
-      auto gray50 = lv_color_make(0x7F, 0x7F, 0x7F);
+
+      //auto white = lv_color_make(0xFF, 0xFF, 0xFF);
+      //auto gray100 = lv_color_make(0xF6, 0xF7, 0xF8);
+      //auto gray200 = lv_color_make(0xEB, 0xEC, 0xEF);
+      auto gray300 = lv_color_make(0xCE, 0xD2, 0xD9);
+      //auto gray400 = lv_color_make(0xB2, 0xB8, 0xC2);
+      auto gray500 = lv_color_make(0x95, 0x9E, 0xAC);
+      //auto gray600 = lv_color_make(0x78, 0x83, 0x96);
+      //auto gray700 = lv_color_make(0x60, 0x6A, 0x7B);
+      auto gray800 = lv_color_make(0x4A, 0x51, 0x5E);
+      //auto gray900 = lv_color_make(0x33, 0x39, 0x42);
+      //auto black = lv_color_make(0x00, 0x00, 0x00);
 
 
-      SetScreenBackground(screen, gray80);
+      auto textColor = gray500;
+      auto arcEmptyColor = gray800;
+      auto arcFillColor = gray300;
+      auto backgroundColor = lv_color_make(0x33, 0x39, 0x42);
 
-      DrawSeperationLine(screen, gray50);
 
-      DrawTitles(screen, gray50);
+      SetScreenBackground(screen, backgroundColor);
 
-      DrawArcs(screen, gray80, gray50);
+      DrawSeperationLine(screen, gray800);
+
+      DrawTitles(screen, textColor);
+
+      DrawArcs(screen, backgroundColor, arcEmptyColor, arcFillColor, textColor);
     }
 
     void DrawSeperationLine(lv_obj_t *parent, lv_color_t lineColor)
@@ -213,7 +226,7 @@ namespace Services
         auto *cpuTitle = lv_label_create(parent);
         lv_label_set_long_mode(cpuTitle, LV_LABEL_LONG_CLIP);
         lv_label_set_text(cpuTitle, "AMD Ryzen 7 5800X");
-        lv_obj_set_size(cpuTitle, ArcWidth + GridSpacing + ArcWidth, TitleHight);
+        lv_obj_set_size(cpuTitle, ArcWidth + GridSpacing + ArcWidth, TitleHeight);
         lv_obj_set_pos(cpuTitle, GridSpacing + 1, GridSpacing + 1);
         lv_obj_add_style(cpuTitle, &TitleStyle, LV_PART_MAIN);
       }
@@ -222,16 +235,16 @@ namespace Services
         auto *gpuTitle = lv_label_create(parent);
         lv_label_set_long_mode(gpuTitle, LV_LABEL_LONG_CLIP);
         lv_label_set_text(gpuTitle, "NVIDIA GeForce RTX 3070");
-        lv_obj_set_size(gpuTitle, ArcWidth + GridSpacing + ArcWidth, TitleHight);
+        lv_obj_set_size(gpuTitle, ArcWidth + GridSpacing + ArcWidth, TitleHeight);
         lv_obj_set_pos(gpuTitle, GridSpacing + ArcWidth + GridSpacing + ArcWidth + GridSpacing + LineThickness + GridSpacing + 1, GridSpacing + 1);
         lv_obj_add_style(gpuTitle, &TitleStyle, LV_PART_MAIN);
       }
     }
 
-    void DrawArcs(lv_obj_t *parent, lv_color_t backgroundColor, lv_color_t emptyColor)
+    void DrawArcs(lv_obj_t *parent, lv_color_t backgroundColor, lv_color_t emptyColor, lv_color_t fillColor, lv_color_t textColor)
     {
-      auto y0 = GridSpacing + TitleHight + 1;
-      auto y1 = GridSpacing + TitleHight + ArcHeight + GridSpacing + 1;
+      auto y0 = GridSpacing + TitleHeight + 1;
+      auto y1 = GridSpacing + TitleHeight + ArcHeight + GridSpacing + 1;
      
       auto x0 = GridSpacing + 1;
       auto x1 = GridSpacing + ArcWidth + GridSpacing + 1;
@@ -239,21 +252,21 @@ namespace Services
       auto x3 = GridSpacing + ArcWidth + GridSpacing + ArcWidth + GridSpacing + LineThickness + GridSpacing + ArcWidth + GridSpacing + 1;
 
 
-      DrawArc(parent, 0, x0, y0, backgroundColor, emptyColor);
-      DrawArc(parent, 1, x1, y0, backgroundColor, emptyColor);
-      DrawArc(parent, 2, x0, y1, backgroundColor, emptyColor);
-      DrawArc(parent, 3, x1, y1, backgroundColor, emptyColor);
+      DrawArc(parent, 0, x0, y0, backgroundColor, emptyColor, fillColor, textColor);
+      DrawArc(parent, 1, x1, y0, backgroundColor, emptyColor, fillColor, textColor);
+      DrawArc(parent, 2, x0, y1, backgroundColor, emptyColor, fillColor, textColor);
+      DrawArc(parent, 3, x1, y1, backgroundColor, emptyColor, fillColor, textColor);
 
-      DrawArc(parent, 4, x2, y0, backgroundColor, emptyColor);
-      DrawArc(parent, 5, x3, y0, backgroundColor, emptyColor);
-      DrawArc(parent, 6, x2, y1, backgroundColor, emptyColor);
-      DrawArc(parent, 7, x3, y1, backgroundColor, emptyColor);
+      DrawArc(parent, 4, x2, y0, backgroundColor, emptyColor, fillColor, textColor);
+      DrawArc(parent, 5, x3, y0, backgroundColor, emptyColor, fillColor, textColor);
+      DrawArc(parent, 6, x2, y1, backgroundColor, emptyColor, fillColor, textColor);
+      DrawArc(parent, 7, x3, y1, backgroundColor, emptyColor, fillColor, textColor);
 
       ArcUpdateEvent.Subscribe(OnArcUpdateEvent);
       Services::System::InvokeLater(&ArcUpdateEvent, ArcUpdateInterval_us, SchedulingBehaviour::FixedPeriodSkipTicks);
     }
 
-    void DrawArc(lv_obj_t *parent, size_t index, lv_coord_t x, lv_coord_t y, lv_color_t backgroundColor, lv_color_t emptyColor)
+    void DrawArc(lv_obj_t *parent, size_t index, lv_coord_t x, lv_coord_t y, lv_color_t backgroundColor, lv_color_t emptyColor, lv_color_t fillColor, lv_color_t textColor)
     {
       auto gaugeStruct = &Gauges[index];
 
@@ -273,7 +286,7 @@ namespace Services
 
 
         lv_style_set_bg_color(indicatorStyle, backgroundColor);
-        lv_style_set_arc_color(indicatorStyle, emptyColor);
+        lv_style_set_arc_color(indicatorStyle, fillColor);
         lv_style_set_arc_width(indicatorStyle, ArcIndicatorSize);
 
         lv_style_set_bg_color(mainStyle, backgroundColor);
@@ -285,27 +298,48 @@ namespace Services
         lv_obj_add_style(arc, indicatorStyle, LV_PART_INDICATOR);
         lv_obj_add_style(arc, mainStyle, LV_PART_MAIN);
 
+        lv_obj_clear_flag(arc, LV_OBJ_FLAG_CLICKABLE);
+
         lv_anim_init(indicatorAnimation);
       }
 
       {
-        auto title = lv_label_create(parent);
+        auto titleLabel = lv_label_create(parent);
         auto titleStyle = &gaugeStruct->TitleStyle;
         auto titleText = gaugeStruct->TitleText;
 
-        gaugeStruct->Title = title;
+        gaugeStruct->TitleLabel = titleLabel;
 
-        lv_label_set_long_mode(title, LV_LABEL_LONG_CLIP);
-        lv_label_set_text(title, titleText);
-        lv_obj_set_size(title, ArcWidth, ArcTextHeight);
-        lv_obj_set_pos(title, x, y + ArcHeight - ArcTextHeight);
+        lv_label_set_long_mode(titleLabel, LV_LABEL_LONG_CLIP);
+        lv_label_set_text(titleLabel, titleText);
+        lv_obj_set_size(titleLabel, ArcWidth, ArcTextHeight);
+        lv_obj_set_pos(titleLabel, x, y + ArcHeight - ArcTextHeight);
 
 
         lv_style_set_text_align(titleStyle, LV_TEXT_ALIGN_CENTER);
         lv_style_set_text_font(titleStyle, &lv_font_montserrat_20);
-        lv_style_set_text_color(titleStyle, emptyColor);
+        lv_style_set_text_color(titleStyle, textColor);
 
-        lv_obj_add_style(title, titleStyle, LV_PART_MAIN);
+        lv_obj_add_style(titleLabel, titleStyle, LV_PART_MAIN);
+      }
+
+      {
+        auto valueLabel = lv_label_create(parent);
+        auto valueStyle = &gaugeStruct->ValueStyle;
+
+        gaugeStruct->ValueLabel = valueLabel;
+
+        lv_label_set_long_mode(valueLabel, LV_LABEL_LONG_CLIP);
+        lv_label_set_text(valueLabel, "");
+
+        lv_obj_set_size(valueLabel, ArcWidth, ValueHeight);
+        lv_obj_set_pos(valueLabel, x, y + ((ArcWidth - ValueHeight)/2));
+
+        lv_style_set_text_align(valueStyle, LV_TEXT_ALIGN_CENTER);
+        lv_style_set_text_font(valueStyle, &lv_font_montserrat_28);
+        lv_style_set_text_color(valueStyle, fillColor);
+
+        lv_obj_add_style(valueLabel, valueStyle, LV_PART_MAIN);
       }
     }
 
@@ -317,18 +351,44 @@ namespace Services
 
         auto minValue = gaugeStruct->MinValue;
         auto maxValue = gaugeStruct->MaxValue;
-        auto nextValue = random(minValue * 1000.0f, maxValue * 1000.0f) / 1000.0f;
+        auto nextValue = random(minValue * 100.0f, maxValue * 100.0f) / 100.0f;
 
+        {
+          auto valueLabel = gaugeStruct->ValueLabel;
+
+
+          if (gaugeStruct->ValueRounding == RoundMode::Integer)
+          {
+            char stringBuilder[10];
+
+            sprintf(stringBuilder, "%.0f ", nextValue);
+            strcat(stringBuilder, gaugeStruct->Unit);
+
+            lv_label_set_text(valueLabel, stringBuilder);
+          }
+
+          if (gaugeStruct->ValueRounding == RoundMode::ThreeDigitsMegaByte)
+          {
+            char stringBuilder[10];
+
+            sprintf(stringBuilder, "%.0f ", nextValue);
+            strcat(stringBuilder, gaugeStruct->Unit);
+
+            lv_label_set_text(valueLabel, stringBuilder);
+          }
+        }
 
         {
           auto arc = gaugeStruct->Arc;
 
           auto currentArcValue = lv_arc_get_value(arc);
-          auto nextArcValue = (int16_t)Math::Map<float>(nextValue, gaugeStruct->MinValue, gaugeStruct->MaxValue, 0.0f, 32767.0f);
+          auto mappedNextValue = Math::Map<float>(nextValue, gaugeStruct->MinValue, gaugeStruct->MaxValue, 0.0f, 32767.0f);
+          auto nextArcValue = (int16_t)Math::Clamp<float>(roundf(mappedNextValue), 0.0f, 32767.0f);
+
 
           auto indicatorAnimation = &gaugeStruct->IndicatorAnimation;
 
-          lv_anim_set_exec_cb(indicatorAnimation, gaugeStruct->UpdateCallback); 
+          lv_anim_set_exec_cb(indicatorAnimation, OnAnimationUpdate); 
           lv_anim_set_values(indicatorAnimation, currentArcValue, nextArcValue);
           lv_anim_set_var(indicatorAnimation, arc);
           lv_anim_set_time(indicatorAnimation, 950);
@@ -339,58 +399,11 @@ namespace Services
       }
     }
 
-    void OnCpuUtilizationUpdate(void *component, int32_t value)
+    void OnAnimationUpdate(void *component, int32_t value)
     {
-      OnAnimationUpdate(&Gauges[0], value);
-    }
+      auto arc = (lv_obj_t*)component;
 
-    void OnCpuWattageUpdate(void *component, int32_t value)
-    {
-      OnAnimationUpdate(&Gauges[1], value);
-    }
-    
-    void OnCpuTemperatureUpdate(void *component, int32_t value)
-    {
-      OnAnimationUpdate(&Gauges[2], value);
-    }
-    
-    void OnCpuMemoryUpdate(void *component, int32_t value)
-    {
-      OnAnimationUpdate(&Gauges[3], value);
-    }
-
-    void OnGpuUtilizationUpdate(void *component, int32_t value)
-    {
-      OnAnimationUpdate(&Gauges[4], value);
-    }
-
-    void OnGpuWattageUpdate(void *component, int32_t value)
-    {
-      OnAnimationUpdate(&Gauges[5], value);
-    }
-    
-    void OnGpuTemperatureUpdate(void *component, int32_t value)
-    {
-      OnAnimationUpdate(&Gauges[6], value);
-    }
-    
-    void OnGpuMemoryUpdate(void *component, int32_t value)
-    {
-      OnAnimationUpdate(&Gauges[7], value);
-    }
-
-    void OnAnimationUpdate(Gauge *gaugeStruct, int32_t arcValue)
-    {
-      auto arc = gaugeStruct->Arc;
-      auto indicatorStyle = &gaugeStruct->IndicatorStyle;
-
-      auto hue = (uint16_t)Math::Map<int16_t>(arcValue, 0, 32767, 105, 0);
-      auto color = lv_color_hsv_to_rgb(hue, 75, 92);
-
-
-      lv_style_set_arc_color(indicatorStyle, color);
-
-      lv_arc_set_value(arc, arcValue);
+      lv_arc_set_value(arc, value);
     }
 
   } // namespace Display
